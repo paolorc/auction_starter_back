8d391044fc63efd6520880d565b19019972994ac
Paolo Reyes <paolo@iovlabs.org>
2021-10-28 00:40:50 -0500

    fix: deploy scripts

---
+++ src/modules/account/entities/account.entity.ts
@@ -0,0 +1,22 @@
+// import { Entity, ObjectID, ObjectIdColumn, Column } from 'typeorm';
+
+// @Entity('users')
+// export class User {
+//   @ObjectIdColumn()
+//   id: ObjectID;
+
+//   @Column()
+//   firstName: string;
+
+//   @Column()
+//   lastName: string;
+
+//   @Column()
+//   email: string;
+
+//   @Column()
+//   phone: string;
+
+//   @Column()
+//   address: string;
+// }
